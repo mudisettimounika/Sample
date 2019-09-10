c663ed9b5c04b00b187500ab369afdc57c1b1d47
mudisettimounika <55123987+mudisettimounika@users.noreply.github.com>
2019-09-10 10:18:06 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,8 @@
+package Salon_web_Reg;
+
+public class Constants {
+	
+	public static String PROPERTIES_PATH = "/home/mounika/eclipse-workspace/Mr_Mrs_Salon_registration/src/test/java/Salon_web_Reg/Text.properties";
+
+
+}
